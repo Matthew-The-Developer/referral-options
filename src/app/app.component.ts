9c34401a4c87cd2d5aeb0fae01044a8d1cbea616
Matthew Watson <Me@MatthewTheDeveloper.com>
2024-02-27 11:15:58 -0500

    - updated the group labels - added options based on feedback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { OptionOneComponent } from './option-one/option-one.component';
 import { OptionTwoComponent } from './option-two/option-two.component';
 import { OptionThreeComponent } from './option-three/option-three.component';
+import { OptionFourComponent } from './option-four/option-four.component';
+import { OptionFiveComponent } from './option-five/option-five.component';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +14,8 @@ import { OptionThreeComponent } from './option-three/option-three.component';
     OptionOneComponent,
     OptionTwoComponent,
     OptionThreeComponent,
+    OptionFourComponent,
+    OptionFiveComponent,
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
---
+++ src/app/option-five/option-five.component.ts
@@ -0,0 +1,101 @@
+import { Component, DestroyRef, effect, inject, signal } from '@angular/core';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { DCINephrologist, Referral, ReferralGroup, ReferralSource } from '../models/referral.model';
+import { adjectives, names, uniqueNamesGenerator } from 'unique-names-generator';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+@Component({
+  selector: 'app-option-five',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './option-five.component.html',
+  styleUrl: './option-five.component.scss'
+})
+export class OptionFiveComponent {
+  private fb = inject(FormBuilder);
+  private destroyRef = inject(DestroyRef);
+
+  group = this.fb.group({
+    referral: new FormControl<Referral | null>(null, [Validators.required]),
+    unknownComment: [{ value: '', disabled: true }],
+  });
+
+  referralValue = signal<Referral | null>(null);
+  referralisUnknown = effect(() => {
+    const source = this.referralValue()?.source;
+    if (
+      source === ReferralSource.Hospital ||
+      source === ReferralSource.PracticeGroup ||
+      source === ReferralSource.NonDCINephrologist ||
+      source === ReferralSource.ToBeDetermined
+    ) {
+      this.group.controls.unknownComment.enable();
+    } else {
+      this.group.controls.unknownComment.reset();
+      this.group.controls.unknownComment.disable();
+    }
+  });
+
+  referralGroups: ReferralGroup[] = [];
+
+  ngOnInit(): void {
+    const referrals: Referral[] = [];
+
+    for(let i = 0; i < 100; i++) {
+      const name = `Dr. ${uniqueNamesGenerator({ dictionaries: [names], style: 'capital' })}`;
+      const location = `${Math.floor(Math.random()*90000) + 10000} - ${uniqueNamesGenerator({ dictionaries: [adjectives], style: 'capital' })} ${uniqueNamesGenerator({ dictionaries: [adjectives], style: 'capital' })}`;
+      
+      referrals.push({
+        source: ReferralSource.DCINephrologist,
+        value: {
+          name,
+          location
+        }
+      })
+    }
+
+    this.referralGroups.push({
+      label: 'DCI-Affiliated Nephrologists',
+      referrals
+    });
+
+    this.referralGroups.push({
+      label: 'Non-DCI Nephrologist',
+      referrals: [
+        {
+          source: ReferralSource.Hospital,
+          value: 'From a Hospital',
+        },
+        {
+          source: ReferralSource.PracticeGroup,
+          value: 'From a Practice Group',
+        },
+        {
+          source: ReferralSource.NonDCINephrologist,
+          value: 'Non-DCI Nephrologist',
+        },
+        {
+          source: ReferralSource.ToBeDetermined,
+          value: 'To Be Determined',
+        },
+      ],
+    });
+
+    this.group.controls.referral.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((value) => this.referralValue.set(value));
+  }
+
+  isDCINephrologist(value: string | DCINephrologist): value is DCINephrologist {
+    return typeof value === 'object' && 'name' in value && 'location' in value;
+  }
+}
